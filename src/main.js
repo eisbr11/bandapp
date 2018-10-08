92b7c659144013d40f6d9741e873daea56faa098
Felix Schliemann <felix.schliemann@netformic.de>
2018-10-08 12:58:37 +0200

    removed element-ui from repo

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import ElementUI from 'element-ui';
 import VueMaterial from 'vue-material';
-import 'element-ui/lib/theme-chalk/index.css';
 import VueLocalStorage from 'vue-localstorage';
 import 'vue-material/dist/vue-material.min.css'
 
-Vue.use(ElementUI);
 Vue.use(VueLocalStorage);
 Vue.use(VueMaterial);
 
